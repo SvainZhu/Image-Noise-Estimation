7055d2eee275a4bd6f3a27d0e2393d575b742f6d
Svain_Zhu <jay13755991665@gmail.com>
2020-03-30 22:35:46 +0800

    添加了使用小波域HH子带的噪音估计算法

--- edge-detection/image-sharpening.py
+++ edge-detection/image-sharpening.py
@@ -1,75 +1,27 @@
+# -*- coding:utf8 -*-
 import cv2
-from pylab import *
+import numpy as np
+import matplotlib.pyplot as plt
 
-saber = cv2.imread("../image/space.jpg")
-#
-gray_saber = cv2.cvtColor(saber, cv2.COLOR_RGB2GRAY)
-gray_saber = cv2.resize(gray_saber, (200, 200))
+# 读取图像
+img = cv2.imread('../image/church.jpg')
+lenna_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
+# 灰度化处理图像
+grayImage = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-def LaplaceOperator(roi, operator_type):
-    if operator_type == "fourfields":
-        laplace_operator = np.array([[0, 1, 0], [1, -4, 1], [0, 1, 0]])
-    elif operator_type == "eightfields":
-        laplace_operator = np.array([[1, 1, 1], [1, -8, 1], [1, 1, 1]])
-    else:
-        raise ("type Error")
-    result = np.abs(np.sum(roi * laplace_operator))
-    return result
+# 拉普拉斯算法
+dst = cv2.Laplacian(grayImage, cv2.CV_16S, ksize=3)
+Laplacian = cv2.convertScaleAbs(dst)
 
+# 用来正常显示中文标签
+plt.rcParams['font.sans-serif'] = ['SimHei']
 
-def LaplaceAlogrithm(image, operator_type):
-    new_image = np.zeros(image.shape)
-    image = cv2.copyMakeBorder(image, 1, 1, 1, 1, cv2.BORDER_DEFAULT)
-    for i in range(1, image.shape[0] - 1):
-        for j in range(1, image.shape[1] - 1):
-            new_image[i - 1, j - 1] = LaplaceOperator(image[i - 1:i + 2, j - 1:j + 2], operator_type)
-    new_image = new_image * (255 / np.max(image))
-    return new_image.astype(np.uint8)
-
-
-def noisy(noise_typ, image):
-    if noise_typ == "gauss":
-        row, col, ch = image.shape
-        mean = 0
-        var = 0.1
-        sigma = var ** 0.5
-        gauss = np.random.normal(mean, sigma, (row, col, ch))
-        gauss = gauss.reshape(row, col, ch)
-        noisy = image + gauss
-        return noisy
-    elif noise_typ == "s&p":
-        row, col, ch = image.shape
-        s_vs_p = 0.5
-        amount = 0.004
-        out = np.copy(image)
-        num_salt = np.ceil(amount * image.size * s_vs_p)
-        coords = [np.random.randint(0, i - 1, int(num_salt))
-                  for i in image.shape]
-        out[coords] = 1
-        num_pepper = np.ceil(amount * image.size * (1. - s_vs_p))
-        coords = [np.random.randint(0, i - 1, int(num_pepper)) for i in image.shape]
-        out[coords] = 0
-        return out
-    elif noise_typ == "poisson":
-        vals = len(np.unique(image))
-        vals = 2 ** np.ceil(np.log2(vals))
-        noisy = np.random.poisson(image * vals) / float(vals)
-        return noisy
-    elif noise_typ == "speckle":
-        row, col, ch = image.shape
-        gauss = np.random.randn(row, col, ch)
-        gauss = gauss.reshape(row, col, ch)
-        noisy = image + image * gauss
-        return noisy
-
-
-plt.subplot(121)
-plt.title("fourfields")
-plt.imshow(LaplaceAlogrithm(gray_saber, "fourfields"), cmap="binary")
-plt.axis("off")
-plt.subplot(122)
-plt.title("eightfields")
-plt.imshow(LaplaceAlogrithm(gray_saber, "eightfields"), cmap="binary")
-plt.axis("off")
+# 显示图形
+titles = [u'原始图像', u'Laplacian算子']
+images = [lenna_img, Laplacian]
+for i in range(2):
+    plt.subplot(1, 2, i + 1), plt.imshow(images[i], 'gray')
+    plt.title(titles[i])
+    plt.xticks([]), plt.yticks([])
 plt.show()
