04d33b55e86e64a7db84f6086d13687b9435d41f
Svain_Zhu <jay13755991665@gmail.com>
2020-03-30 22:17:52 +0800

    修改了Canny算子检测图像边缘算法

--- edge-detection/Canny.py
+++ edge-detection/Canny.py
@@ -1,34 +1,127 @@
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
+import math
 import cv2
-from pylab import *
 
-saber = cv2.imread("../image/church.jpg")
-#
-gray_saber = cv2.cvtColor(saber, cv2.COLOR_RGB2GRAY)
-gray_saber = cv2.resize(gray_saber, (200, 200))
+img = plt.imread('../image/church.jpg')
 
+sigma1 = sigma2 = 1
+sum = 0
 
-def GaussianOperator(roi):
-    GaussianKernel = np.array([[1, 2, 1], [2, 4, 2], [1, 2, 1]])
-    result = np.sum(roi * GaussianKernel / 16)
-    return result
+gaussian = np.zeros([5, 5])
+for i in range(5):
+    for j in range(5):
+        # 生成二维高斯分布矩阵
+        gaussian[i, j] = math.exp(-1 / 2 * (np.square(i - 3) / np.square(sigma1) + (np.square(j - 3) / np.square(sigma2)))) / (2 * math.pi * sigma1 * sigma2)
 
+        sum = sum + gaussian[i, j]
 
-def GaussianSmooth(image):
-    new_image = np.zeros(image.shape)
-    image = cv2.copyMakeBorder(image, 1, 1, 1, 1, cv2.BORDER_DEFAULT)
-    for i in range(1, image.shape[0] - 1):
-        for j in range(1, image.shape[1] - 1):
-            new_image[i - 1, j - 1] = GaussianOperator(image[i - 1:i + 2, j - 1:j + 2])
-    return new_image.astype(np.uint8)
+gaussian = gaussian / sum
 
 
-smooth_saber = GaussianSmooth(gray_saber)
-plt.subplot(121)
-plt.title("Origin Image")
+# print(gaussian)
+
+
+# step1.高斯滤波
+gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+W, H = gray.shape
+new_gray = np.zeros([W - 5, H - 5])
+for i in range(W - 5):
+    for j in range(H - 5):
+        new_gray[i, j] = np.sum(gray[i:i + 5, j:j + 5] * gaussian)  # 与高斯矩阵卷积实现滤波
+
+
+
+
+# step2.增强 通过求梯度幅值
+W1, H1 = new_gray.shape
+dx = np.zeros([W1 - 1, H1 - 1])
+dy = np.zeros([W1 - 1, H1 - 1])
+d = np.zeros([W1 - 1, H1 - 1])
+for i in range(W1 - 1):
+    for j in range(H1 - 1):
+        dx[i, j] = new_gray[i, j + 1] - new_gray[i, j]
+        dy[i, j] = new_gray[i + 1, j] - new_gray[i, j]
+        d[i, j] = np.sqrt(np.square(dx[i, j]) + np.square(dy[i, j]))  # 图像梯度幅值作为图像强度值
+
+
+
+
+# setp3.非极大值抑制 NMS
+W2, H2 = d.shape
+NMS = np.copy(d)
+NMS[0, :] = NMS[W2 - 1, :] = NMS[:, 0] = NMS[:, H2 - 1] = 0
+for i in range(1, W2 - 1):
+    for j in range(1, H2 - 1):
+
+        if d[i, j] == 0:
+            NMS[i, j] = 0
+        else:
+            gradX = dx[i, j]
+            gradY = dy[i, j]
+            gradTemp = d[i, j]
+
+            # 如果Y方向幅度值较大
+            if np.abs(gradY) > np.abs(gradX):
+                weight = np.abs(gradX) / np.abs(gradY)
+                grad2 = d[i - 1, j]
+                grad4 = d[i + 1, j]
+                # 如果x,y方向梯度符号相同
+                if gradX * gradY > 0:
+                    grad1 = d[i - 1, j - 1]
+                    grad3 = d[i + 1, j + 1]
+                # 如果x,y方向梯度符号相反
+                else:
+                    grad1 = d[i - 1, j + 1]
+                    grad3 = d[i + 1, j - 1]
+
+            # 如果X方向幅度值较大
+            else:
+                weight = np.abs(gradY) / np.abs(gradX)
+                grad2 = d[i, j - 1]
+                grad4 = d[i, j + 1]
+                # 如果x,y方向梯度符号相同
+                if gradX * gradY > 0:
+                    grad1 = d[i + 1, j - 1]
+                    grad3 = d[i - 1, j + 1]
+                # 如果x,y方向梯度符号相反
+                else:
+                    grad1 = d[i - 1, j - 1]
+                    grad3 = d[i + 1, j + 1]
+
+            gradTemp1 = weight * grad1 + (1 - weight) * grad2
+            gradTemp2 = weight * grad3 + (1 - weight) * grad4
+            if gradTemp >= gradTemp1 and gradTemp >= gradTemp2:
+                NMS[i, j] = gradTemp
+            else:
+                NMS[i, j] = 0
+
+
+
+
+# step4. 双阈值算法检测、连接边缘
+W3, H3 = NMS.shape
+DT = np.zeros([W3, H3])
+# 定义高低阈值
+TL = 0.1 * np.max(NMS)
+TH = 0.3 * np.max(NMS)
+for i in range(1, W3 - 1):
+    for j in range(1, H3 - 1):
+        if (NMS[i, j] < TL):
+            DT[i, j] = 0
+        elif (NMS[i, j] > TH):
+            DT[i, j] = 1
+        elif ((NMS[i - 1, j - 1:j + 1] < TH).any() or (NMS[i + 1, j - 1:j + 1]).any()
+              or (NMS[i, [j - 1, j + 1]] < TH).any()):
+            DT[i, j] = 1
+
+dst = 255 - DT
+#plt.imshow(new_gray, cmap=cm.gray)
+#plt.imshow(d, cmap=cm.gray)
+#plt.imshow(NMS, cmap=cm.gray)
+plt.imshow(dst, cmap=cm.gray)
 plt.axis("off")
-plt.imshow(gray_saber, cmap="gray")
-plt.subplot(122)
-plt.title("GaussianSmooth Image")
-plt.axis("off")
-plt.imshow(smooth_saber, cmap="gray")
 plt.show()
